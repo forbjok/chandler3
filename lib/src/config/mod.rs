5d9d187235b30155f26400c9e3226135385aabd7
Forb.Jok <forbjok@gmail.com>
2020-12-06 03:43:16 +0100

    chore: Use "dirs_next" crate instead of the abandoned "dirs"

--- lib/src/config/mod.rs
+++ lib/src/config/mod.rs
@@ -15,7 +15,7 @@ pub use self::regex::*;
 pub const CONFIG_DIR: &str = "chandler3";
 
 pub fn get_config_path() -> Option<PathBuf> {
-    let config_path = dirs::config_dir().map(|p| p.join(CONFIG_DIR));
+    let config_path = dirs_next::config_dir().map(|p| p.join(CONFIG_DIR));
 
     if config_path.is_none() {
         error!("Could not get configuration path!");
