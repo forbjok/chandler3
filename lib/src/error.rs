25565f5b4159ac19bdf48db34d386a0d26dc39ce
Forb.Jok <forbjok@gmail.com>
2022-11-28 18:05:22 +0100

    refactor: Use anyhow

--- lib/src/error.rs
+++ lib/src/error.rs
@@ -3,8 +3,6 @@ use std::io;
 
 use thiserror::Error;
 
-use crate::util;
-
 #[derive(Debug)]
 pub enum DownloadError {
     Http { code: u16, description: Cow<'static, str> },
@@ -19,7 +17,7 @@ pub enum ChandlerError {
     #[error("Error loading project")]
     LoadProject(Cow<'static, str>),
     #[error("Error opening config")]
-    OpenConfig(util::FileError),
+    OpenConfig(anyhow::Error),
     #[error("Error reading config")]
     ReadConfig(io::Error),
     #[error("Error parsing config")]
@@ -27,9 +25,9 @@ pub enum ChandlerError {
     #[error("Configuration error")]
     Config(Cow<'static, str>),
     #[error("Error opening file")]
-    OpenFile(util::FileError),
+    OpenFile(anyhow::Error),
     #[error("Error creating file")]
-    CreateFile(util::FileError),
+    CreateFile(anyhow::Error),
     #[error("Error reading file")]
     ReadFile(io::Error),
     #[error("Error writing file")]
